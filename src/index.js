35f65eac29522feffb7ba15228604183ad466c74
DavidHuang <david.huang3214@gmail.com>
2023-07-05 13:00:47 -0700

    Add page styling, tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 
 import loadHeader from "./scripts/header";
 import loadHome from "./scripts/home";
-import loadMenu from "./scripts/menu";
-import loadContact from "./scripts/contact";
 import loadListeners from "./scripts/listeners";
 
 import './css/styles.css';
 
 loadHeader();
-loadContact();
+loadHome();
 loadListeners();
